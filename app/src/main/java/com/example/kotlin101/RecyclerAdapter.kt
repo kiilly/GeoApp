5b18122e0dfe954512d0e7f1039719d1be654448
abdelqodousse boustani <bousta_a@etna-alternance.net>
2023-02-16 10:25:26 +0100

    2 etapes avec un leger soucis d'affichage sur la card

---
+++ app/src/main/java/com/example/kotlin101/RecyclerAdapter.kt
@@ -0,0 +1,29 @@
+/*package com.example.kotlin101
+
+import android.view.TextureView
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.RecyclerView
+/*class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+    inner class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var itemImage: ItemView
+        var itemTitle: TextView
+        var itemDescription : TextView
+    }
+}*/
